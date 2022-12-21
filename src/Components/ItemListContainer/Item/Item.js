a123a7cb85384914824db65fd9e8d5d91c213508
Claudio Vilas <claudiogvilas@gmail.com>
2022-12-21 12:45:15 -0300

    favoritos

--- src/Components/ItemListContainer/Item/Item.js
+++ src/Components/ItemListContainer/Item/Item.js
@@ -18,7 +18,9 @@ const Item = ({listados}) => {
               <p className='mostrar'>{listado.nombre}</p>
               <p className='mostrar'>USD {listado.valor}</p><br></br>
               <button className = 'botoncards'><Link to = {`/listado/${listado.id}`} >Ver Detalle</Link></button>
-              <img  onClick = { ()=> { addFavorito (listado) } } src = {'./imagenes/botonfavorito.jpg'} className = 'favorito' ></img>
+              <img  onClick = { ()=> { addFavorito (listado) } } src = {!favorito ? './imagenes/botonfavorito.jpg' : './imagenes/agregadofavoritos.jpg'} className = 'favorito' />
+              {/* <img  onClick = { ()=> { addFavorito (listado) } } src = {'./imagenes/botonfavorito.jpg'} className = 'favorito' ></img> */}
+
             </div>
            )) }
           </div>
